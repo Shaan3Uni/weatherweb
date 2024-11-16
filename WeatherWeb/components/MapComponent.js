c2240cc431aa88072d0fd3cdba7dac15bc451281
Ryland <rylandhill42@gmail.com>
2024-11-16 03:55:49 -0500

    gift for dhruv

--- WeatherWeb/components/MapComponent.js
+++ WeatherWeb/components/MapComponent.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
+import 'leaflet/dist/leaflet.css';
 
 const MapComponent = () => {
-  useEffect(() => {
-    import('leaflet/dist/leaflet.css');
-  }, []);
+    
+
 
   const { MapContainer, TileLayer, Marker, Popup } = require('react-leaflet');
 
@@ -20,11 +20,11 @@ const MapComponent = () => {
       <Marker position={[43.461249, -80.517577]}>
         <Popup>
             <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0e738c17-7f3c-422e-8225-f8c782b08626/df2r1el-90bed6c3-aa87-4d42-9120-0ab731b1ea26.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBlNzM4YzE3LTdmM2MtNDIyZS04MjI1LWY4Yzc4MmIwODYyNlwvZGYycjFlbC05MGJlZDZjMy1hYTg3LTRkNDItOTEyMC0wYWI3MzFiMWVhMjYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.y17nc91l3mjY4laTfdpNz0Y_6U2o3xdoi6nuvNSklhQ" style={{width:50, height:'auto'}}/>
-            
         </Popup>
       </Marker>
     </MapContainer>
   );
 };
+
 
 export default MapComponent;
