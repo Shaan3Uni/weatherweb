41e0e43e3aca8e218e41f209a5133a79019b2fb2
shaan-nair <shaannair06@gmail.com>
2024-11-16 05:41:38 -0500

    me again

--- WeatherWeb/screens/MapScreen.js
+++ WeatherWeb/screens/MapScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import MapComponent from '@/components/MapComponent';
 
 const MapScreen = () => (
   <View style={styles.container}>
-    <Text>Map Screen</Text>
+    <MapComponent />
   </View>
 );
 
