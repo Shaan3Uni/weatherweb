fe914d6d06f1f8bfacf15b25f713de19c23b349e
Ryland <rylandhill42@gmail.com>
2024-11-16 08:27:00 -0500

    fuck my butt

--- WeatherWeb/components/MapComponent.js
+++ WeatherWeb/components/MapComponent.js
@@ -1,30 +1,79 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'leaflet/dist/leaflet.css';
 
+// URLs for icons
+const greenIconUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Map_pin_icon_green.svg/752px-Map_pin_icon_green.svg.png';
+const redIconUrl = 'https://www.freeiconspng.com/uploads/red-location-map-pin-icon-5.png';
+const blackIconUrl = 'https://cdn-icons-png.flaticon.com/512/484/484167.png';
+const yellowIconUrl = 'https://cdn-icons-png.flaticon.com/512/7107/7107955.png';
+
+const createMarker = (lat, lng, name, phone, sev, description) => {
+  return { lat, lng, name, phone, sev, description };
+};
+
 const MapComponent = () => {
-    
+  const [L, setL] = useState(null);
+  const [customIcons, setCustomIcons] = useState({});
+  const [markers, setMarkers] = useState([]);
 
+  useEffect(() => {
+    // Ensure window is available
+    if (typeof window !== 'undefined') {
+      import('leaflet').then((module) => {
+        const leaflet = module.default;
+        setL(leaflet);
 
-  const { MapContainer, TileLayer, Marker, Popup } = require('react-leaflet');
+        // Initialize icons
+        const icons = {
+          1: leaflet.icon({ iconUrl: blackIconUrl, iconSize: [25, 25], iconAnchor: [12, 41], popupAnchor: [1, -34] }),
+          2: leaflet.icon({ iconUrl: redIconUrl, iconSize: [25, 25], iconAnchor: [12, 41], popupAnchor: [1, -34] }),
+          3: leaflet.icon({ iconUrl: yellowIconUrl, iconSize: [25, 25], iconAnchor: [12, 41], popupAnchor: [1, -34] }),
+          4: leaflet.icon({ iconUrl: greenIconUrl, iconSize: [25, 25], iconAnchor: [12, 41], popupAnchor: [1, -34] }),
+        };
+        setCustomIcons(icons);
+      });
+    }
+  }, []);
 
+  function AddMarker() {
+    useMapEvents({
+      click(e) {
+        const lat = e.latlng.lat;
+        const lng = e.latlng.lng;
+        const name = prompt('Please enter your name:');
+        const phone = prompt('Please input your phone number:');
+        const sev = prompt('How severe is this emergency? (1-3):');
+        const description = prompt('Please give a brief description of your emergency:');
+
+        if (name && phone && sev && description) {
+          const newMarker = createMarker(lat, lng, name, phone, parseInt(sev)+1, description);
+          setMarkers((prevMarkers) => [...prevMarkers, newMarker]);
+        }
+      },
+    });
+    return null;
+  }
+
+  if (!L || !customIcons[1]) return null;
+  const { MapContainer, TileLayer, Marker, Popup, useMapEvents, useMap } = require('react-leaflet');
   return (
-    <MapContainer
-      center={[43.461249, -80.517577]}
-      zoom={13}
-      style={{ height: '500px', width: '100%' }}
-    >
+    <MapContainer center={[43.461249, -80.517577]} zoom={13} style={{ height: '500px', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[43.461249, -80.517577]}>
-        <Popup>
-            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0e738c17-7f3c-422e-8225-f8c782b08626/df2r1el-90bed6c3-aa87-4d42-9120-0ab731b1ea26.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBlNzM4YzE3LTdmM2MtNDIyZS04MjI1LWY4Yzc4MmIwODYyNlwvZGYycjFlbC05MGJlZDZjMy1hYTg3LTRkNDItOTEyMC0wYWI3MzFiMWVhMjYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.y17nc91l3mjY4laTfdpNz0Y_6U2o3xdoi6nuvNSklhQ" style={{width:50, height:'auto'}}/>
-        </Popup>
-      </Marker>
+      <AddMarker />
+      {markers.map((marker, index) => (
+        <Marker key={index} position={[marker.lat, marker.lng]} icon={customIcons[marker.sev]}>
+          <Popup>
+            <strong>{marker.name}</strong><br />
+            {marker.phone}<br />
+            {marker.description}
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
-
 
 export default MapComponent;
